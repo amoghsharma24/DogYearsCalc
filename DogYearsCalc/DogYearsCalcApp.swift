241518fbb7106fa9468c33fd9e09cd25cd4e342e
Amogh Sharma <amoghsharma02@gmail.com>
2025-02-20 10:55:26 +1100

    Initial Commit

---
+++ DogYearsCalc/DogYearsCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogYearsCalcApp.swift
+//  DogYearsCalc
+//
+//  Created by Amogh Sharma  on 20/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct DogYearsCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
