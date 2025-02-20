f13315058fe3c3b792dd2330a7f049dfb9622eda
Amogh Sharma <amoghsharma02@gmail.com>
2025-02-20 11:32:47 +1100

    Built the dog calculator

--- DogYearsCalc/ContentView.swift
+++ DogYearsCalc/ContentView.swift
@@ -8,14 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var age = 0.0
+    
+    func calculateDogYears() -> Int {
+        let conversionAge = 7.0
+        
+        return Int(age * conversionAge)
+    }
+    
     var body: some View {
+        
+        ZStack{
+            Color.accentColor
+                .ignoresSafeArea()
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            switch age {
+            case 0...15:
+                Text("🐶")
+                    .font(.system(size: 90))
+            case 15...20:
+                Text("🐺")
+                    .font(.system(size: 90))
+            case 20...25:
+                Text("🪦")
+                    .font(.system(size: 90))
+            default:
+                Text("🐕‍🦺")
+                    .font(.system(size: 90))
+            }
+            
+            Text("Dog Years Calculator!")
+                .font(.largeTitle)
+                .fontDesign(.rounded)
+                .fontWeight(.black)
+            Slider(value: $age, in: 0...24, step: 1)
+            
+            HStack{
+                    Text("My age: \(Int(age))")
+                        .fontWeight(.black)
+                    
+                    Text("Dog's Age: \(calculateDogYears())")
+                        .fontWeight(.black)
+                        
+            }
         }
         .padding()
+        }
     }
 }
 
