cc17d1aee417c7e038a5ae112e51624775dc4f67
Amogh Sharma <amoghsharma02@gmail.com>
2025-02-20 11:46:03 +1100

    chande background colour

--- DogYearsCalc/ContentView.swift
+++ DogYearsCalc/ContentView.swift
@@ -20,7 +20,7 @@ struct ContentView: View {
     var body: some View {
         
         ZStack{
-            Color.accentColor
+            Color.teal
                 .ignoresSafeArea()
         
         VStack {
